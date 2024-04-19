6c0af82f810d535ecb6865ac01483591133143ad
Александр Рахмаев <rahmaevao@engee.cloud>
2024-04-19 22:16:02 +0300

    Гимны - песни

--- content_generator/content_generator.py
+++ content_generator/content_generator.py
@@ -4,4 +4,4 @@ from src.notes_converter import NotesConverter
 
 
 if __name__ == "__main__":
-    NotesConverter().convert(Path("music_notes"), Path("content/Песни"))
+    NotesConverter().convert(Path("music_notes"), Path("content/Гимны"))
