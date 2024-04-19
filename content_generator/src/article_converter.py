8d12abd208372deea28a552be94754138640d9c4
Александр Рахмаев <rahmaevao@engee.cloud>
2024-04-19 21:33:08 +0300

    Тест работет на конвертацию

--- content_generator/src/article_converter.py
+++ content_generator/src/article_converter.py
@@ -1,20 +1,58 @@
+import re
+
+
+class NotConversableNoteError(Exception):
+    pass
+
 class ArticleConverter:
+    _NEED_LEN_RAW_PROP = 2
+
     def __init__(self):
         pass
 
     def convert(self, md_text: str) -> str:
-        obsidian_tags = self._get_obsidian_tags(md_text)
+        body = md_text.split("---")[2]
+        tags = self.__get_tags(body)
+        if "#raw" in tags:
+            raise NotConversableNoteError
+        obsidian_properties = self._get_obsidian_properties(md_text)
+        for tag in tags:
+            body = body.replace(tag, "")
+        body = self._get_hat(obsidian_properties) + body
+        return body
 
-    def _get_obsidian_tags(self, md_text: str) -> dict:
-        raw_tags = md_text.split("---")[1].split("---")[0]
-        tags: dict = {}
-        for raw_tag in raw_tags.split("\n"):
-            raw_tags_items = raw_tag.split(": ")
-            if len(raw_tags_items) < 2:
+    def _get_obsidian_properties(self, md_text: str) -> dict:
+        raw_properties = md_text.split("---")[1].split("---")[0]
+        properties: dict = {}
+        for raw_property in raw_properties.split("\n"):
+            raw_properties_items = raw_property.split(": ")
+            if len(raw_properties_items) < self._NEED_LEN_RAW_PROP:
                 continue
-            key = raw_tags_items[0]
-            value = raw_tags_items[1]
+            key = raw_properties_items[0]
+            value = raw_properties_items[1]
             if value.startswith('"[[') and value.endswith(']]"'):
                 value = value[3:-3]
-            tags[key] = value
-        return tags
+            properties[key] = value
+        return properties
+    
+    def __get_tags(self, body: str) -> list[str]:
+        return re.findall(r"#\w+", body)
+
+    def _get_hat(self, properties: dict) -> str:
+        hat = (
+            f'Title: {properties["title"]}\n'
+            "Date: 1993-06-12\n"
+            "Categories: Гимны\n\n"            
+        )
+        mapping = {
+            "origianl_title": "Оригинальное название",
+            "poet": "Поэт",
+            "translater": "Переводчик",
+            "composer": "Композитор",
+            "song_of_rebirth": "Песнь возрождения",
+            "published": "Дата написания",
+        }
+        for property in properties:
+            if property in mapping:
+                hat += f"{mapping[property]}: {properties[property]}\n\n"
+        return hat
--- content_generator/tests/test_article_converter.py
+++ content_generator/tests/test_article_converter.py
@@ -27,26 +27,28 @@ published: 1901
 
 """
 
-_EXPECTED_MD_TEXT = """
-Title: Господня вся земля
-Date: 2024-01-01
+_EXPECTED_MD_TEXT = """Title: Господня вся земля
+Date: 1993-06-12
 Categories: Гимны
 
 Оригинальное название: This Is My Father's World
 
 Поэт: Maltbie Davenport Babcock
 
-Переводчик: [Даниил Александрович Ясько]({filename}persons/dd.md)
+Переводчик: Даниил Александрович Ясько
 
 Композитор: Franklin L. Sheppard
 
 Песнь возрождения: 2507
 
-Дата написания: 1901 г.
+Дата написания: 1901
+
+
+
 
 ## История написания
 
-Некоторая история
+Некоторая история.
 
 ## Интересные факты
 
@@ -56,10 +58,11 @@ Categories: Гимны
 
 - [Hymnary.org](https://hymnary.org/text/this_is_my_fathers_world_and_to_my)
 - [Wikipedia](https://en.wikipedia.org/wiki/This_Is_My_Father%27s_World)
+
 """
 
 
 def test_base_converting():
     converter = ArticleConverter()
-
-    assert converter.convert(_INPUT_TEST_MD_TEXT) == "test"
+    print(converter.convert(_INPUT_TEST_MD_TEXT))
+    assert converter.convert(_INPUT_TEST_MD_TEXT) == _EXPECTED_MD_TEXT
